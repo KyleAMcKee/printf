fec7886f1ef6c27fcd173e5695658ec55132f843
kmckee <kmckee@student.42.fr>
2017-12-03 14:54:28 -0800

    Int format code more readable and compact

--- srcs/int_format.c
+++ srcs/int_format.c
@@ -6,11 +6,35 @@
 /*   By: kmckee <kmckee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 18:34:19 by kmckee            #+#    #+#             */
-/*   Updated: 2017/12/03 12:22:43 by kmckee           ###   ########.fr       */
+/*   Updated: 2017/12/03 14:50:43 by kmckee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+t_type	set_flags(t_type type, int len)
+{
+//	if (type.flags.precision && type.w_precision <= len)
+//		type.flags.zero = 0;
+	type.width -= len;
+	if (type.flags.left == 1)
+	{
+		type.flags.right = 0;
+		type.flags.zero = 0;
+	}
+	if (type.flags.zero == 1)
+		type.flags.right = 0;
+	if (type.w_precision > type.width)
+		type.width = 0;
+	if (type.result.num_jug < 0 || type.flags.plus)
+		type.width--;
+	if (type.w_precision > len)
+		type.w_precision -= len;
+	else
+		type.w_precision = 0;
+	type.width -= type.w_precision;
+	return (type);
+}
 
 int	num_len(intmax_t val)
 {
@@ -32,23 +56,23 @@ int	num_len(intmax_t val)
 int	int_format(t_type type, va_list ap)
 {
 	int total;
-	int i;
+	int digits;
 
 	type = arg_conversion(type, ap);
-	total = num_len(type.result.num_jug);
-	i = total;
-	if (type.flags.precision && type.w_precision <= i)
-		type.flags.zero = 0;
-	if (type.w_precision > type.width)
-		type.width = 0;
-	i += prepend_space(type, total);
-	i += prepend_zero(type, total);
-	if (!type.width && !type.w_precision)
-		i += display_sign(type);
+	digits = num_len(type.result.num_jug);
+	total = digits;
+	type = set_flags(type, digits);
+	//print_status(type);
+	total += prepend_space(type, digits);
+	total += display_sign(type);
+	total += prepend_zero(type, digits);
+//	print_status(type);
+//	if (!type.width && !type.w_precision)
+//		i += display_sign(type);
 	if (type.flags.precision == 1 && type.w_precision == 0 && type.result.num_jug == 0)
-		return (type.width > 0 ? i - 1 : 0);
+		return (type.width > 0 ? total - 1 : 0);
 	print_max(type.result.num_jug);
 	if (type.flags.left == 1)
-		i += right_justify(type, total);
-	return (i);
+		total += right_justify(type, total);
+	return (total);
 }
