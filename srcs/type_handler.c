00c01952da533101b13f60ecb8492890fc63ee5b
kmckee <kmckee@student.42.fr>
2017-12-04 15:32:03 -0800

    testing string issue

--- srcs/type_handler.c
+++ srcs/type_handler.c
@@ -6,7 +6,7 @@
 /*   By: kmckee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/25 21:02:32 by kmckee            #+#    #+#             */
-/*   Updated: 2017/12/04 15:29:28 by kmckee           ###   ########.fr       */
+/*   Updated: 2017/12/04 15:31:48 by kmckee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 int	type_handler(t_type type, va_list ap)
 {
-	ft_putchar(type.type);
-	ft_putnbr(type.length.l);
-	ft_putchar('*');
-	ft_putnbr(type.length.ll);
-	ft_putchar('*');
 	if (type.type == '%')
 		return (percent_format(type));
 	if (type.type == 'c' && type.length.l != 1)
 		return (char_format(type, ap)); 
 	if (type.type == 'C' || (type.type == 'c' && type.length.l == 1))
 		return (wchar_format(type, ap));
-	if (type.type == 's')
-		return (string_format(type, ap));
 	if (type.type == 'S' || (type.type == 's' && type.length.l == 1))
 		return (wchar_string_format(type, ap));
+	if (type.type == 's')
+		return (string_format(type, ap));
 	if (type.type == 'i' || type.type == 'd' || type.type == 'D')
 		return (int_format(type, ap));
 	if (type.type == 'x' || type.type == 'X')
