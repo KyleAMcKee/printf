ef0f8c068059d745c754782df0b72d0531e1007e
kmckee <kmckee@student.42.fr>
2017-12-04 14:49:12 -0800

    testing int issue

--- srcs/check_flags.c
+++ srcs/check_flags.c
@@ -6,7 +6,7 @@
 /*   By: kmckee <kmckee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/25 15:32:55 by kmckee            #+#    #+#             */
-/*   Updated: 2017/12/04 14:46:42 by kmckee           ###   ########.fr       */
+/*   Updated: 2017/12/04 14:48:19 by kmckee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,12 +95,12 @@ t_type	check_flags(const char *str, t_type type, int *i)
 			type.ret++;
 		}
 	}
-	if (type.flags.zero > 1)
+	/*if (type.flags.zero > 1)
 	{
 		ft_putchar('^');
 		type.flags.zero = 0;
 		type.flags.right = 1;
-	}
+	}*/
 	*i += 1;
 	return (type);
 }
