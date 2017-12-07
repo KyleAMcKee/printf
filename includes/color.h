fdc86d59ec1c8834e887e72de6a5d0deae759d9a
kmckee <kmckee@student.42.fr>
2017-12-06 19:30:21 -0800

    Adding color functionality

---
+++ includes/color.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmckee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/20 11:28:33 by kmckee            #+#    #+#             */
+/*   Updated: 2017/12/06 16:18:24 by kmckee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOR_H
+# define COLOR_H
+
+# define RED		"\x1b[31m"
+# define ORANGE		"\x1b[38;2;255;128;0m"
+# define YELLOW		"\x1b[33m"
+# define GREEN		"\x1b[32m"
+# define BLUE		"\x1b[34m"
+# define MAGENTA	"\x1b[35m"
+# define PURPLE		"\x1b[38;2;127;0;255m"
+# define CYAN		"\x1b[36m"
+# define PINK		"\x1b[38;2;255;102;178m"
+# define HALLOWEEN	"\x1b[38;2;0;0;0;1;5;48;2;252;127;0m"
+# define CHRISTMAS	"\x1b[32;1;5;41m"
+# define END		"\x1b[0m"
+
+#endif
