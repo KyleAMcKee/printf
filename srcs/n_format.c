441c92f32fd1c5b822f89e20099a942aea03169f
kmckee <kmckee@student.42.fr>
2017-12-07 14:52:09 -0800

    %n usage fixed

--- srcs/n_format.c
+++ srcs/n_format.c
@@ -6,7 +6,7 @@
 /*   By: kmckee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 13:45:43 by kmckee            #+#    #+#             */
-/*   Updated: 2017/12/07 14:28:31 by kmckee           ###   ########.fr       */
+/*   Updated: 2017/12/07 14:51:21 by kmckee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	n_format(t_type type, va_list ap)
 {
 	type = arg_conversion(type, ap);
-	type.res.intp = &type.ret;
-	ft_putnbr(*type.res.intp);
+	*type.res.intp = type.ret;
 	return (0);
 }
